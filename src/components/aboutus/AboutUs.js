37d68309d8e3ccd8c6fe75131fb8ec033414f552
JacopoLuri <geomjacopoluri@gmail.com>
2020-12-17 18:51:56 +0100

    about half done

--- src/components/aboutus/AboutUs.js
+++ src/components/aboutus/AboutUs.js
@@ -3,25 +3,14 @@ import { Context } from "../../context/Context";
 import { ApiContext } from "../../context/ApiContext";
 
 const AboutUs = () => (
-    <Context.Consumer>
-        {(value) => (
-            <ApiContext.Consumer>
-                {(api) => (
-                    <>
-                        <Navbar />
-                        <h1>I AM AboutUs</h1>
-                        <button onClick={value.test}>
-                            context function run example. i console log the world example
-                        </button>
-                        <button onClick={api.test}>
-                            api context function run example. i console log the world
-                            apicontext
-                        </button>
-                    </>
-                )}
-            </ApiContext.Consumer>
-        )}
-    </Context.Consumer>
+    <>
+        <Navbar />
+            <a href="" target="_blank">
+            <img src="https://www.pngfind.com/pngs/m/94-945926_github-love-github-hd-png-download.png" />
+            <p>Roxana Florea</p>
+            </a>
+        
+    </>
 );
 
 export default AboutUs;
