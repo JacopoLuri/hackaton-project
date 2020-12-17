215663b39ae85bc05bf8cca536f1e20745f9f9cc
Befa222 <72620186+Befa222@users.noreply.github.com>
2020-12-17 23:52:38 +0000

    tutorial page

--- src/components/tutorial/Tutorial.js
+++ src/components/tutorial/Tutorial.js
@@ -11,12 +11,12 @@ const Tutorial = () => (
         
         <div className="intro-text">
         <h1 className="title-intro">Welcometh thee not restful traveleth'r</h1>
-            <p> Raiseth an image of thee, h're!</p>
-            <p> To witness the witchcraft and charm.</p>
-            <p> One wilt raiseth an image.</p>
-            <p> From thine mem'ry boxeth.</p>
-            <p> So as to seeth the col'r appeareth wh're </p>
-            <p> Black hath used to beest </p>
+            <p> Raiseth an image of thee, h're!
+            To witness the witchcraft and charm.
+            One wilt raiseth an image.
+            From thine mem'ry boxeth.
+            So as to seeth the col'r appeareth wh're. 
+            Black hath used to beest.</p>
             </div>
     </div>
 );
