5afbaa891ca6432c4b9cfe465340ed9a4d51bff4
JacopoLuri <geomjacopoluri@gmail.com>
2020-12-17 20:15:05 +0100

    solving conflicts

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,17 +6,14 @@ import { slide as Menu } from "react-burger-menu";
 
 const Navbar = (props) => {
     return(
-   <Menu {...props}>
-    <Link to="/">HOME</Link>
-    <Link to="/page1">Tutorial</Link>
-    <Link to="/page2">Take A Picture</Link>
-    <Link to="/page3">About Us</Link>
-   </Menu>
-    )
-    
+        <Menu {...props}>
+            <Link to="/">HOME</Link>
+            <Link to="/page1">Tutorial</Link>
+            <Link to="/page2">Take A Picture</Link>
+            <Link to="/page3">About Us</Link>
+        </Menu>
+    ) 
 }
-    /* IF YOU CHANGE THE LINK TO... REMEMBER TO CHANGE IT ON APP.JS ROUTE AS WELL. */
-   
 ;
 
 export default Navbar;
