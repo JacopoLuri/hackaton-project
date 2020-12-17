96c5c9c7c7588c4b2e081f4282428bebaa720f7f
Befa222 <72620186+Befa222@users.noreply.github.com>
2020-12-17 23:38:40 +0000

    tutorial page

--- src/components/tutorial/Tutorial.js
+++ src/components/tutorial/Tutorial.js
@@ -1,10 +1,24 @@
 import Navbar from "../navbar/Navbar";
+import '../tutorial/Tutorial.css'
+
 
 const Tutorial = () => (
-    <>
+    <div className="tuto-frame">
+        
         <Navbar />
-        <h1>I AM Tutorial</h1>
-    </>
+        
+        
+        
+        <div className="intro-text">
+        <h1 className="title-intro">Welcometh thee not restful traveleth'r</h1>
+            <p> Raiseth an image of thee, h're!</p>
+            <p> To witness the witchcraft and charm.</p>
+            <p> One wilt raiseth an image.</p>
+            <p> From thine mem'ry boxeth.</p>
+            <p> So as to seeth the col'r appeareth wh're </p>
+            <p> Black hath used to beest </p>
+            </div>
+    </div>
 );
 
 export default Tutorial;
